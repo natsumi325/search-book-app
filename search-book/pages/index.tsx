b5339c77bec22537c5060844a2002357c9b5050d
natsumi325 <jiyama06@gmail.com>
2022-11-13 00:02:47 +0900

    🎉 初期構築

---
+++ search-book/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Button, Text } from '@mantine/core'
+
+export default function Home() {
+  return (
+    <div>
+      <Box>
+        <Button>ボタンだよ</Button>
+        <Text>テキストだよ</Text>
+      </Box>
+    </div>
+  )
+}
